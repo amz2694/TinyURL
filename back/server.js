6e55763d20d879d4b4d7e2c0fd3138e745d41795
amz2694 <alimiree.zade@gmail.com>
2022-03-23 14:52:32 +0430

    mongoose

--- back/server.js
+++ back/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = process.PORT || 8080;
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConfig');
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+require('dotenv').config();
+
+connectDB();
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+})
