c61a1cf8efd91a3e25b37bdd1f69576fcdb039f4
amz2694 <alimiree.zade@gmail.com>
2022-03-24 15:55:28 +0430

    connect GET /short api

---
+++ front/src/views/Redirect.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <h1> heeeeeeeeeeeey {{ shortURL }}</h1>
+    <h1 @click="redirectTo">click</h1>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            shortURL : this.$route.params.shortURL
+        }
+    },
+    methods : {
+        async redirectTo() {
+            console.log(this.shortURL);
+            await axios
+                .get('http://localhost:8000/short',{ params: {URL: this.shortURL}})
+                .then(res => {
+                    window.location.href = res.data.realURL;
+                })
+                .catch(err => {
+                    console.log(JSON.stringify(err));
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
