5b5073cea73c49316cc14b8aba51f52c30c91871
amz2694 <alimiree.zade@gmail.com>
2022-03-25 10:00:45 +0430

    cors stuff

---
+++ back/config/allowedOrigins.js
@@ -0,0 +1,15 @@
+const allowedOrigins = [
+    'https://www.mysite.com',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:8080',
+    'http://localhost:8080',
+    'http://localhost:8000',
+    'http://192.168.1.106:8080/',
+    'https://127.0.0.1:8000',
+    'https://127.0.0.1:8080',
+    'https://localhost:8080',
+    'https://localhost:8000',
+    'https://192.168.1.106:8080/'
+];
+
+module.exports = allowedOrigins
--- back/config/corsOption.js
+++ back/config/corsOption.js
@@ -1,18 +1,16 @@
-const whitelist = [
-    'https://www.mysite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:8080'
-];
+const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
         }
     },
+    credentials: true,
     optionsSuccessStatus: 200
 }
+
 
 module.exports = corsOptions;
