b4035da495e6ebff7d50874b473acb26c990b8c2
amz2694 <alimiree.zade@gmail.com>
2022-03-29 14:56:26 +0430

    redirect style

--- front/src/views/Redirect.vue
+++ front/src/views/Redirect.vue
@@ -1,7 +1,9 @@
 <template>
 <div>
-    <h1> heeeeeeeeeeeey {{ shortURL }}</h1>
-    <h1 @click="redirectTo">click</h1>
+    <div class="container">
+        <span class="timer" id="timer" v-if="!msg">{{ timer }}</span>
+        <span class="msg" v-if="msg">{{ msg }}</span>
+    </div>
 </div>
 </template>
 
@@ -10,25 +12,55 @@ import axios from 'axios'
 export default {
     data() {
         return {
-            shortURL : this.$route.params.shortURL
+            msg : '',
+            intervalVar : null,
+            timer : 5,
+        }
+    },
+    mounted() {
+        this.interval()
+    },
+    watch : {
+        $route(to,from) {
+            if (to.name == "redirect") {
+                this.interval()
+            }
         }
     },
     methods : {
+        interval () {
+            this.msg = '';
+            this.intervalVar = setInterval(() => {
+                this.timer -= 1;
+                if (this.timer<=0) {
+                    this.timer = 5;
+                    this.redirectTo();
+                    clearInterval(this.intervalVar);
+                }
+            }, 1000);
+        },
         async redirectTo() {
-            console.log(this.shortURL);
+            const shortURL = this.$route.params.shortURL;
+            console.log(shortURL);
             await axios
-                .get('http://localhost:8000/short',{ params: {URL: this.shortURL}})
+                .get('https://localhost:8000/short',{ params: {URL: shortURL}})
                 .then(res => {
+                    this.msg = "Redirecting ...";
                     window.location.href = res.data.realURL;
                 })
                 .catch(err => {
-                    console.log(JSON.stringify(err));
+                    if (err.response.status == 404) {
+                        this.msg = "shortURL not found";
+                    } else {
+                        this.msg = "something went wrong please try again";
+                        console.log(err);
+                    }
             })
         }
     }
 }
 </script>
 
-<style>
+<style src="../style/redirect.css" scoped>
 
 </style>
