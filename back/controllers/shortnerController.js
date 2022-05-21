fc3db1452989bd7dae3edf43fb67fbbed37008a3
amz2694 <alimiree.zade@gmail.com>
2022-05-22 00:15:50 +0430

    new method for shortner

--- back/controllers/shortnerController.js
+++ back/controllers/shortnerController.js
@@ -1,49 +1,38 @@
+const { base } = require('../model/Url');
 const Url = require('../model/Url');
+
+const CHAR = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const BASE = CHAR.length;
+const basenum = 10000001;
+const reversedNum = num => parseFloat(num.toString().split('').reverse().join('')) * Math.sign(num);
 
 const handleCreateShortner = async (req,res) => {
     let { URL } = req.body;
     if (!URL ) return res.status(400).json({ "message": "URL is required." });
     try {
-        let last = await Url.find().sort({ _id: -1 }).limit(1);
-        if (!last[0]) {
-            if (URL.indexOf("://") == -1) {
-                URL = "://".concat(URL);
-            }
-            const result = await Url.create({
-                "user": req.user,
-                "realURL": URL,
-                "shortURL": 'aaaaaa',
-            })
-            console.log(result);
-            res.status(201).json({ "shortURL": 'aaaaaa' });
-        } else {
-            let lastId = last[0].shortURL
-            const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"];
-            let i = lastId.length - 1;
-            let change = alphabet.indexOf(lastId[i]);
-            let rest = '';
-
-            while (change == alphabet.length - 1) {
-                i--;
-                change = alphabet.indexOf(lastId[i])
-            }
-            let when = (lastId.length - 1 - i)
-            for (let j = 0; j < when; j++) {
-                rest += alphabet[0];
-            }
-            short = lastId.slice(0, i) + alphabet[change + 1] + rest;
-            if (URL.indexOf("://") == -1) {
-                URL = "://".concat(URL);
-            }
-            // create && store the new user
-            const result = await Url.create({
-                "user": req.user,
-                "realURL": URL,
-                "shortURL": short,
-            })
-            console.log(result);
-            res.status(201).json({ "shortURL": short });
-            }
+        const results = await Url.find().sort({ _id: -1 }).limit(1);
+        const lastResult = results[0];
+        let lastId = lastResult.id;
+        let id = lastId + 1;
+        let num = basenum + id;
+        let int = reversedNum(num);
+        let short = ''
+        do {
+            short = CHAR[int % BASE] + short
+            int = Math.floor(int / BASE)
+        } while (int > 0)
+        if (URL.indexOf("://") == -1) {
+            URL = "://".concat(URL);
+        }
+        // create && store the new user
+        const result = await Url.create({
+            "id": id,
+            "user": req.user,
+            "realURL": URL,
+            "shortURL": short,
+        })
+        console.log(result);
+        res.status(201).json({ "shortURL": short });
     } catch (err) {
         console.log(err)
         res.status(500).json({ "message": err.message });
@@ -53,7 +42,19 @@ const handleCreateShortner = async (req,res) => {
 const handleGetShortner = async (req,res) => {
     const URL = req.query.URL;
     if (!URL ) return res.status(400).json({ "message": "URL is required." });
-    const foundURL = await Url.findOne({ shortURL : URL }).exec();
+    let result = 0;
+    for (let char of URL.split('')) {
+        const d = CHAR.indexOf(char)
+        if (d >= 0) {
+            result = result * BASE + d
+        } else {
+            console.log('err');
+            res.sendStatus(500);
+        }
+    }
+    let reverseId = reversedNum(result);
+    let realId = reverseId - basenum;
+    const foundURL = await Url.findOne({ id : realId }).exec();
     if (foundURL) {
         res.json({ "realURL": foundURL.realURL });
     } else {
--- back/model/Url.js
+++ back/model/Url.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const urlSchema = new Schema({
+    id: {
+        type: Number,
+        required: true
+    },
     user: {
         type: String,
         required: true
